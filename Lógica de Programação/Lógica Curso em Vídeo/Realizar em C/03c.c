2c39033dfdd3e5329104f0d61d1dde5890b1a06f
MarcosGabriel123 <msantiagorodrigues487@gmail.com>
2026-02-25 16:42:25 -0400

    concluido IMC

--- Lógica de Programação/Lógica Curso em Vídeo/Realizar em C/03c.c
+++ Lógica de Programação/Lógica Curso em Vídeo/Realizar em C/03c.c
@@ -14,8 +14,17 @@ int main (){
     else if (imc >= 18.5 && imc < 25){
         printf("PESO NORMAL!");
     }
-    else{
+    else if (imc >= 25 && imc < 30){
         printf("SOBREPESO!");
+    }
+    else if (imc >= 30 && imc < 35){
+        printf("OBESIDADE GRAU 1");
+    }
+    else if (imc >= 35 && imc < 40 ){
+        printf("OBESIDADE GRAU 2");
+    }
+    else {
+        printf("OBESIDADE GRAU 3");
     }
 
     return 0;
