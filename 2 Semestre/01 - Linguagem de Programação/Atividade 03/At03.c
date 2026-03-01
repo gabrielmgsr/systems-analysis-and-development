e259ad42e00799a597b14115413c626a1a743f5d
MarcosGabriel123 <msantiagorodrigues487@gmail.com>
2026-02-28 23:23:32 -0400

    Atividade 03

--- 2 Semestre/01 - Linguagem de Programação/Atividade 03/At03.c
+++ 2 Semestre/01 - Linguagem de Programação/Atividade 03/At03.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 
 int main() {
-    int M, N, i, j;
+    int matriz[3][3];
+    int i, j;
+    int soma_total = 0;
+    int soma_diagonal = 0;
+    int maior, menor;
 
-    printf("Quantas linhas vai ter a matriz?");
-    scanf("%d", &M);
-    printf("Quantas colunas vai ter a matriz?");
-    scanf("%d", &N);
+    printf("Digite os valores da matriz 3x3:\n");
 
-    int mat[M][N];
-
-    for (i = 0; i < M; i++){
-        for (j = 0; j < N; j++){
-            printf("Elemento [%d, %d]: ", i,j);
-            scanf("%d", &mat[i][j]);
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
         }
     }
 
-    printf("\nMATRIZ DIGITADA\n");
-    for (i = 0; i < M; i++){
-        for (j = 0; j < N; j++){
-            printf("%d ", mat[i][j]);
+    maior = matriz[0][0];
+    menor = matriz[0][0];
+
+    printf("\nMatriz digitada:\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%4d ", matriz[i][j]);
+
+            soma_total += matriz[i][j];
+
+            if(matriz[i][j] > maior)
+                maior = matriz[i][j];
+
+            if(matriz[i][j] < menor)
+                menor = matriz[i][j];
+
+            if(i == j)
+                soma_diagonal += matriz[i][j];
         }
         printf("\n");
     }
+
+    printf("\nResultados:\n");
+    printf("Soma de todos os elementos: %d\n", soma_total);
+    printf("Maior valor: %d\n", maior);
+    printf("Menor valor: %d\n", menor);
+    printf("Soma da diagonal principal: %d\n", soma_diagonal);
 
     return 0;
 }
